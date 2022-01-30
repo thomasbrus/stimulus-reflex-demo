fe1cdb634718424cae8f7276c6a52da78443472a
Thomas Brus <thomas.brus@me.com>
2022-01-30 16:07:06 +0100

    Couple small fixes

--- app/reflexes/todos_reflex.rb
+++ app/reflexes/todos_reflex.rb
@@ -7,7 +7,7 @@ class TodosReflex < ApplicationReflex
   end
 
   def save
-    todo = GlobalID::Locator.locate_signed(element.dataset.sgid)
+    todo = GlobalID::Locator.locate_signed(element.dataset.sgid, only: [Todo])
     todo.update(params.require(:todo).permit(:description))
     morph dom_id(todo, :saved_at), render(partial: 'shared/saved_at', locals: { model: todo })
   end
